ce5e015de3d824c1064f68c8ce2abfdf2b0e1cd5
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-03 21:44:35 -0200

    places github link closer to bottom

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>luls</p>
-          <a
-            className="App-link"
-            href="https://github.com/fabioaromanini/site"
-            target="_blank"
-            rel="noopener noreferrer"
-          ><img src={gitIcon} alt="github repository"/>
-          </a>
+        <header id="landing">
+          <div className="links">
+            <a
+              href="https://github.com/fabioaromanini/site"
+              target="_blank"
+              rel="noopener noreferrer"
+            ><img src={gitIcon}  alt="github repository icon" />
+            </a>
+            <span>github</span>
+          </div>
         </header>
       </div>
     );
