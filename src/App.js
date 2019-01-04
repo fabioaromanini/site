73d75b432c526ceb83302ccec91bafc9016948a3
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-03 22:37:31 -0200

    adds content

--- src/App.js
+++ src/App.js
@@ -8,19 +8,29 @@ class App extends Component {
     return (
       <div className="App">
         <header id="landing">
-          <h1>Fabio Augusto Romanini Pinto</h1>
+          <h1>fabioaromanini</h1>
+          <div id="landing-content">
+            <p>An{' '}
+            <a
+                href="https://github.com/fabioaromanini/site"
+                target="_blank"
+                rel="noopener noreferrer"
+                >open source
+            </a>
+            {' '}personal site built on react and google cloud</p>
+          </div>
           <div className="links">
             <a
-              href="https://github.com/fabioaromanini/site"
+              href="https://github.com/fabioaromanini"
               target="_blank"
               rel="noopener noreferrer"
-            ><img src={gitIcon} alt="github repository icon" />
+              ><img src={gitIcon} alt="github repository icon" />
             </a>
             <a
               href="https://www.linkedin.com/in/fabioaromanini/"
               target="_blank"
               rel="noopener noreferrer"
-            ><img src={linkedinIcon} alt="linkedin icon" />
+              ><img src={linkedinIcon} alt="linkedin icon" />
             </a>
           </div>
         </header>
