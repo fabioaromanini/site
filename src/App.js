4b2bc601719ff7cca7822d75a7668393f8f81629
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-03 22:12:55 -0200

    adds linkedin button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import gitIcon from './giticon.png';
+import linkedinIcon from './linkedinicon.png';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +14,12 @@ class App extends Component {
               target="_blank"
               rel="noopener noreferrer"
             ><img src={gitIcon}  alt="github repository icon" />
-            github
+            </a>
+          <a
+              href="https://www.linkedin.com/in/fabioaromanini/"
+              target="_blank"
+              rel="noopener noreferrer"
+            ><img src={linkedinIcon}  alt="linkedin icon" />
             </a>
           </div>
         </header>
