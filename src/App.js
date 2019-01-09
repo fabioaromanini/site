9519d5079d87d84b3c7477a5727c07f6d039b69a
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-08 23:41:45 -0200

    updates site content

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
                 >under development
             </a>
             {' '}personal website, written in react
-            {' '}and hosted on google cloud platform.
+            {' '}hosted on aws s3,
+            {' '}delivered using cloud front.
             </p>
           </div>
           <div className="links">
