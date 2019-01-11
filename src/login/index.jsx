4ac70c74ddde771ebcabaa1c572afb41b867d0f1
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-11 20:08:08 -0200

    removes as much style as posible from login.css

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -5,68 +5,69 @@ import linkedinIcon from './images/linkedinicon.png';
 
 import './login.css';
 
-const styles = {
-  login: {
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  loginForm: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    minHeight: '40vh',
-    minWidth: '70vw',
-    paddingRight: '15vw',
-  },
-  loginField: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    minWidth: '40vw',
-  },
-  loginInput: {
-    minWidth: '40vw',
-    marginLeft: '3vw',
-  },
-};
+export default () => {
+  const styles = {
+    login: {
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      flexDirection: 'column',
+    },
+    loginField: {
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      minWidth: '40vw',
+    },
+    loginInput: {
+      minWidth: '40vw',
+      marginLeft: '3vw',
+    },
+    links: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-evenly',
+    },
+    linksImg: {
+      maxHeight: '8vh',
+    },
+  };
 
-const inputs = [
-  <div className="loginField" style={styles.loginField}>
-    <label htmlFor="email">email</label>
-    <input style={styles.loginInput} id="email" type="email" />
-  </div>,
-  <div className="loginField" style={styles.loginField}>
-    <label htmlFor="password">password</label>
-    <input style={styles.loginInput} id="password" type="password" />
-  </div>,
-];
+  const inputs = [
+    <div className="loginField" style={styles.loginField}>
+      <label htmlFor="email">email</label>
+      <input style={styles.loginInput} id="email" type="email" />
+    </div>,
+    <div className="loginField" style={styles.loginField}>
+      <label htmlFor="password">password</label>
+      <input style={styles.loginInput} id="password" type="password" />
+    </div>,
+  ];
 
-export default () => (
-  <div id="login" style={styles.login}>
-    <h1>Login</h1>
-    <form>
-      <div id="loginForm" style={styles.loginForm}>
-        {inputs}
-      </div>
-    </form>
-    <div className="links">
-      <a
-        href="https://github.com/fabioaromanini/site"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={gitIcon} alt="github repository icon" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/fabioaromanini/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={linkedinIcon} alt="linkedin icon" />
-      </a>
+  const links = [
+    <a
+      href="https://github.com/fabioaromanini/site"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img style={styles.linksImg} src={gitIcon} alt="github repository icon" />
+    </a>,
+    <a
+      href="https://www.linkedin.com/in/fabioaromanini/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img style={styles.linksImg} src={linkedinIcon} alt="linkedin icon" />
+    </a>,
+  ];
+
+  return (
+    <div id="login" style={styles.login}>
+      <h1>Login</h1>
+      <form>
+        <div id="loginForm">{inputs}</div>
+      </form>
+      <div style={styles.links}>{links}</div>
     </div>
-  </div>
-);
+  );
+};
