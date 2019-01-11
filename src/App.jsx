d077adcab0ab5817171579985321ea0bb1f7cfcd
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-11 18:23:20 -0200

    extends landing colors to whole app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 import Landing from './landing';
 import Login from './login';
 
+const styles = {
+  app: {
+    backgroundColor: '#483538',
+    color: 'white',
+  },
+};
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.app}>
         <Landing />
         <Login />
       </div>
