920a9d10f6b7cae4dd85dbece382f555ca665c4d
Fabio Romanini <fabioaromanini@gmail.com>
2019-01-11 20:12:54 -0200

    removes as much style as posible from landing.css

--- src/landing/index.jsx
+++ src/landing/index.jsx
@@ -3,24 +3,40 @@ import React from 'react';
 
 import './landing.css';
 
-export default () => (
-  <div id="landing">
-    <h1>fabioaromanini</h1>
-    <div className="content">
-      <p>
-        A playground for some technologies and concepts I'm studying. Currently,
-        this website is:
-      </p>
-      <ul>
-        <li>mobile first</li>
-        <li>written in react</li>
-        <li>built and deployed with travis</li>
-        <li>hosted on aws s3</li>
-        <li>distributed using cloud front</li>
-        <li>
-          <a href="#login">login using cognito</a>
-        </li>
-      </ul>
+export default () => {
+  const styles = {
+    landing: {
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '0 10vw',
+    },
+    landingTitle: {
+      marginBottom: '15vh',
+    },
+  };
+
+  return (
+    <div id="landing" style={styles.landing}>
+      <h1 style={styles.landingTitle}>fabioaromanini</h1>
+      <div className="content">
+        <p>
+          A playground for some technologies and concepts I'm studying.
+          Currently, this website is:
+        </p>
+        <ul>
+          <li>mobile first</li>
+          <li>written in react</li>
+          <li>built and deployed with travis</li>
+          <li>hosted on aws s3</li>
+          <li>distributed using cloud front</li>
+          <li>
+            <a href="#login">login using cognito</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
