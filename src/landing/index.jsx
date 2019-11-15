270d8153c478ca41fecf2b16279988ece28b29a1
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-15 11:44:16 -0200

    adds picture to landing

--- src/landing/index.jsx
+++ src/landing/index.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import arrow from './images/gif-bounce-arrow.gif';
 import { Container, Row, Col } from 'reactstrap';
+
+import arrow from './images/gif-bounce-arrow.gif';
+import avatar from './images/avatar.jpeg';
 
 import './landing.css';
 
 export default () => (
   <Container id="landing">
+    <Row>
+      <Col xs="12" id="avatar">
+        <img src={avatar} alt="fabio romanini" />
+      </Col>
+    </Row>
     <Row>
       <Col xs="12" id="title">
         <h1>fabioaromanini</h1>
